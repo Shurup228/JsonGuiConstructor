40ce9e207833aeb649fb66e383db3b0f020fe80a
yarik <yroslav541@gmail.com>
2018-03-19 10:22:04 +0200

    Basic grammar

--- library/jsonToQt.py
+++ library/jsonToQt.py
@@ -1,5 +1,16 @@
+from typing import Dict
+
+from PyQt5.Qt import QObject, QLayout
+from PyQt5.QtWidgets import QFormLayout, QVBoxLayout, QHBoxLayout, QGridLayout
 from PyQt5.QtWidgets import QMainWindow
-mapping = {
-    "Window": QMainWindow,
+
+widgets: Dict[str, QObject] = {
+    "QMainWindow": QMainWindow
 }
 
+layouts: Dict[str, QLayout] = {
+    "QFormLayout": QFormLayout,
+    "QVBoxLayout": QVBoxLayout,
+    "QHBocLayout": QHBoxLayout,
+    "QGridLayout": QGridLayout
+}
